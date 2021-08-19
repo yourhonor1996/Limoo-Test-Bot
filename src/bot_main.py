827d63aec67dc2e382605f847a32bbd589a50cd0
Mohammd Alavi <yourhonor1996@gmail.com>
2021-08-20 03:58:14 +0430

    Did These: - improve /gitlab-projects command to show different visibilities - create help command - improve settings structure and literals management

--- src/bot_main.py
+++ src/bot_main.py
@@ -4,6 +4,7 @@ from src.config import settings
 from src.gitlab_async_api import GitlabAsyncConnection
 import aiohttp
 from src.util import utility
+from src.config.settings import Commands, GitlabSettings
 
 # TODO creat an event handler class 
 # TODO create conveniece classes for this shit ass sdk
@@ -11,34 +12,48 @@ from src.util import utility
 
 async def respond(event, session):
         data = event['data']
-
         if (event['event'] == 'message_created' and not (event['data']['message']['type'] or event['data']['message']['user_id'] == self['id'])):
             message_id = event['data']['message']['id']
             thread_root_id = event['data']['message']['thread_root_id']
             direct_reply_message_id = thread_root_id and event['data']['message']['id']
 
-            if event['data']['message']['text'].startswith("/gitlab-projects"):
-                token = event['data']['message']['text'].split()[1]
-                # create a connection to the gilab api- the user_id is stored in the api
-                connection = await GitlabAsyncConnection.create(session, token)
-                # get all the private projects using the user id and the given parameters
-                projects = await connection.get_data(f'/users/{connection.user_id}/projects', parameters={'visibility':'private'})
-
-                results = "Here are your private projects:\n"
-                filters = settings.PROJECTS_FIELD_FILTERS
-                for i, project in enumerate(projects):
-                    results += f"Project No. {i+1}: "
-                    response_text = utility.format_dict_to_text(project, filters)
-                    results += response_text
-                    # results.append(utility.filter_dict(project, filters))
+            # if we have a gitlab project command ...
+            if event['data']['message']['text'].startswith(Commands.GITLAB_PROJECTS):
                 
+                message_split = event['data']['message']['text'].split()
+                visibility = message_split[1]
+                token = message_split[2]
+                
+                if not visibility in GitlabSettings.VALID_VISIBILITIES:
+                    response_text = GitlabSettings.TEXT_INVALID_COMMAND
+                else:
+                    # create a connection to the gilab api- the user_id is stored in the api
+                    connection = await GitlabAsyncConnection.create(session, token)
+                    if visibility == 'all':
+                    # get all the private projects using the user id and the given parameters
+                        projects = await connection.get_data(f'/users/{connection.user_id}/projects')
+                    else:
+                        projects = await connection.get_data(f'/users/{connection.user_id}/projects', parameters={'visibility':visibility})
+                    
+                    response_text = "***Here are your projects:***\n"
+                    filters = GitlabSettings.PROJECTS_FIELD_FILTERS
+                    for i, project in enumerate(projects):
+                        response_text += f"**{i+1} - {project['name']}:**\n"
+                        project_text = utility.format_dict_to_text(project, filters)
+                        response_text += project_text
+                    
+                    response = await ld.messages.create(
+                        workspace_id= data['workspace_id'],
+                        conversation_id= event['data']['message']['conversation_id'],
+                        text= response_text)
             
-            response = await ld.messages.create(
-                workspace_id= data['workspace_id'],
-                conversation_id= event['data']['message']['conversation_id'],
-                text= results,
-                thread_root_id= thread_root_id or message_id,
-                direct_reply_message_id= direct_reply_message_id)
+            # if we have a help command
+            if event['data']['message']['text'].startswith(Commands.HELP):
+                text= Commands.HELP_TEXT
+                response = await ld.messages.create(
+                    workspace_id= data['workspace_id'],
+                    conversation_id= event['data']['message']['conversation_id'],
+                    text= text)
             
 
 async def main():
--- src/config/settings.py
+++ src/config/settings.py
@@ -19,6 +19,25 @@ BOT_USERNAME = 'test-bot1'
 
 BOT_PASSWORD = 'tyz2jsps2xo9ba2ck3ok'
 
-PROJECTS_FIELD_FILTERS = ['id', 'name','web_url']
+
 
 REQ_FILENAME = 'requirements.txt'
+
+class Commands():
+    GITLAB_PROJECTS = "/gitlab-projects"
+    HELP = "/help"
+    HELP_TEXT = """سلام این یک بات تست هست.
+با استفاده از دستوری های زیر میتونید از این بات استفاده بکنید:
+/gitlab-projects : 
+بعد از نوشتن این دستور باید نوع پروژه هایی که میخواید مشاهده بکنید رو بنویسید.
+یعنی یکی از سه عبارت "private" – "public" – "internal" و یا "all".
+هر کدوم از سه عبارت بالا باعث میشن انواع پروژه های مربوط با این visibility ها برای شما نشون داده بشه و عبارت all هم تمام پروژه های شما رو براتون لیست میکنه.
+بعد از نوشتن نوع پروژه ها باید gitlab private access token خودتون رو وارد کنید.
+برای مثال:
+/gitlab-pojects private <<your token>>
+"""
+
+class GitlabSettings:
+    PROJECTS_FIELD_FILTERS = ['id', 'name','web_url', 'visibility']
+    VALID_VISIBILITIES = ['public', 'private', 'internal', 'all']
+    TEXT_INVALID_COMMAND = "دستور وارد شده به شکل صحیح نمیباشد. برای اطلاع از نحوه نوشتن دستورات دستور /help را اجرا کنید."
